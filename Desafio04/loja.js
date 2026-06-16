e47e974757c99ac6f3825cab8a71fd0290626a37
thepamela0393 <pab.0393@hotmail.com>
2026-06-15 21:50:51 -0300

    desafio 04 em processo

---
+++ Desafio04/loja.js
@@ -0,0 +1,19 @@
+/*
+
+DESAFIO 04 - LOJA TEWCH
+Uma loja de informática possui vários produtos cadastrados.
+
+Crie um array de objetos contendo:
+- nome
+- preço
+
+Utilize uma estrutura de repetição para mostrar apenas os produtos que custam mais de R$100,00.
+Exiba o nome dos produtos encontrados.
+*/
+let produtos [
+{"Cartão de memória" , preco 120},
+{"CPU" , preco 450},
+{"fone" , preco 80},
+{"Monitor" , preco 560}
+]
+for ( let i = 0; i <produtos.length; i++)
