113c1dae9f15418fb7a0596bece0f8b3d708b000
thepamela0393 <pab.0393@hotmail.com>
2026-06-15 21:03:48 -0300

    Desafio02 concluido

--- Desafio02/media.js
+++ Desafio02/media.js
@@ -11,9 +11,12 @@ Média turma: 10.0
 let notas =  [7.4 , 8.1 , 5.2 , 9.0, 9.5];
     let somar = 0
 
-for (i = 0;i <= notas.length; i++){
-    somar =+ notas[i];
+for (i = 0;i < notas.length; i++){
+    somar += notas[i];
 }
+let media = somar / notas.length;
+console.log("Soma das notas:", somar);
+console.log("Média da turma:", media);
 
 
 
